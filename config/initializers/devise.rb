b98227ad51005eeafc371306fcdfae83986f1611
John Bert <jbertlik@gmail.com>
2018-09-06 18:56:57 -0400

    changed devise key for heroku deployment

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,8 +8,8 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '67233901e01c772386f36c43ebe0c79aa5afdf8e1b757394b25c2612d11541c764a15598ad49d11090eb8b2bc1564ab15ac7b685442df573ad63a75fa3a14d10'
-  
+  # config.secret_key = ENV['DEVISE_KEY']
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'jbertlik@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
